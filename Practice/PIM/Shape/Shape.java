bb0a14d5930cd8f8981f3174544fdfec367e9582
SharKPonG <Pingkiesdev@gmail.com>
2025-08-11 19:22:16 +0700

    TestShape

---
+++ Practice/PIM/Shape/Shape.java
@@ -0,0 +1,13 @@
+package Practice.PIM.Shape;
+
+// interface class  implements
+// abstract class
+// static class  
+public interface Shape {
+
+    // method
+    public double calculateArea();
+    
+    public void setArea(double area);
+
+}
